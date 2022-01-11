a295e99e83a4f56de3e50bf5b73d1dfb8274d9e8
ewelinabid <s205980@student.dtu.dk>
2022-01-11 15:34:10 +0100

    some changes

--- s1_getting_started/exercise_files/final_exercise/model.py
+++ s1_getting_started/exercise_files/final_exercise/model.py
@@ -4,3 +4,16 @@ from torch import nn
 class MyAwesomeModel(nn.Module):
     def __init__(self):
         super().__init__()
+        self.hidden1 = nn.Sequential(nn.Linear(784,256),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=0.2))
+        self.hidden2 = nn.Sequential(nn.Linear(256,128),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=0.2)),
+        self.fc1 = nn.Sequential(nn.Linear(128,10))
+        
+    def forward(self, x):
+        x = x.view(x.shape[0],-1)
+        x = self.hidden1(x)
+        x = self.hidden2(x)
+        return self.fc1(x)
